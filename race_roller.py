c262bbfd36353f59ce5ddda0ea6638bf1537bd59
cyberusfaustus <cyberusfaustus@gmail.com>
2021-08-18 10:19:18 -0500

    Add race_roller test and refactored race_roller to be more Functional

--- race_roller.py
+++ race_roller.py
@@ -4,130 +4,174 @@ from xdice import *
 
 primary_roll  = roll("1d100")
 
-primary_table = {
-        "HUMAN":50,
-        "DWARF":60,
-        "ELF":70,
-        "HALFLING":80,
-        "GNOME":84,
-        "HALF-ELF":88,
-        "GOLIATH":90,
-        "DRAGONBORN":92,
-        "HALF-ORC":94,
-        "TIEFLING":96,
-        "AASIMAR":98,
-        "AARAKOCRA":99,
-        "GENASI":100
-        }
+phb_race_table = (
+	{"RACE":"Dragonborn","LOW":1,"HIGH":1},
+	{"RACE":"Half-Elf","LOW":2,"HIGH":3},
+	{"RACE":"Halfling","LOW":4,"HIGH":6},
+	{"RACE":"Elf","LOW":7,"HIGH":9},
+	{"RACE":"Human","LOW":10,"HIGH":89},
+	{"RACE":"Dwarf","LOW":90,"HIGH":94},
+	{"RACE":"Gnome","LOW":95,"HIGH":97},
+	{"RACE":"Half-Orc","LOW":98,"HIGH":99},
+	{"RACE":"Tiefling","LOW":100,"HIGH":100}
+	)
 
-def human_sub():
-    sub = roll("1d10")
-    if sub > 6:
-        return "Specialist (Variant)"
-    return "Generalist (Standard)"
+def race(rroll, rtable):
+	prace = ""
+	for r in rtable:
+	    if rroll >= r["LOW"] and rroll <= r["HIGH"]:
+	        prace += r["RACE"]	
+	return prace
 
-def dwarf_sub():
-    sub = roll ("1d10")
-    if sub <= 2:
-        return "Gray"
-    elif sub <= 5:
-        return "Mountain"
-    return "Hill"
+def subrace(prace):
+    srace = ""
+    srace += dragonborn_sub(prace)
+    srace += half_elf_sub(prace)
+    srace += halfling_sub(prace)
+    srace += elf_sub(prace)
+    srace += human_sub(prace)
+    srace += dwarf_sub(prace)
+    srace += gnome_sub(prace)
+    ##srace += half_orc_sub(prace)
+    srace += tiefling_sub(prace) 
+    return srace
 
-def elf_sub():
-    sub = roll ("1d8")
-    if sub == 1:
-        return "Eladrin"
-    elif sub <= 3:
-        return "Drow"
-    elif sub <= 5:
-        return "Wood"
-    return "High"
 
-def halfling_sub():
-    sub = roll("1d10")
-    if sub == 1:
-        return "Ghostwise"
-    elif sub <= 5:
-        return "Stout"
-    return "Lightfoot"
+def human_sub(prace):
+    if prace != "Human":
+        return ""
+    else:
+        sub = roll("1d10")
+        if sub > 6:
+            return "Specialist (Variant)"
+        return "Generalist (Standard)"
 
-def gnome_sub():
-    sub = roll("1d10")
-    if sub <= 2:
-        return "Deep"
-    elif sub <= 5:
-        return "Forest"
-    return "Rock"
+def dwarf_sub(prace):
+    if prace != "Dwarf":
+        return ""
+    else:
+        sub = roll ("1d10")
+        if sub <= 2:
+           return "Gray"
+        elif sub <= 5:
+           return "Mountain"
+        return "Hill"
 
-def half_elf_sub():
-    sub = roll("1d20")
-    if sub <= 2:
-        return "Half Eladrin"
-    elif sub <= 4:
-        return "Half Drow"
-    elif sub <= 8:
-        return "Half Wood Elf"
-    elif sub <= 13:
-        return "Half High Elf"
-    return "Unkown Elven heritage"
+def elf_sub(prace):
+    if prace != "Elf":
+        return ""
+    else:
+        sub = roll ("1d8")
+        if sub == 1:
+            return "Eladrin"
+        elif sub <= 3:
+            return "Drow"
+        elif sub <= 5:
+            return "Wood"
+        return "High"
 
-def dragonborn_color():
-    sub = roll("1d10")
-    if sub == 1:
-        return "Black"
-    elif sub == 2:
-        return "Blue"
-    elif sub == 3:
-        return "Brass"
-    elif sub == 4:
-        return "Bronze"
-    elif sub == 5:
-        return "Copper"
-    elif sub == 6:
-        return "Gold"
-    elif sub == 7:
-        return "Green"
-    elif sub == 8:
-        return "Red"
-    elif sub == 9:
-        return "Silver"
-    return "White"
+def halfling_sub(prace):
+    if prace != "Halfling":
+        return ""
+    else:
+        sub = roll("1d10")
+        if sub == 1:
+            return "Ghostwise"
+        elif sub <= 5:
+            return "Stout"
+        return "Lightfoot"
 
+def gnome_sub(prace):
+    if prace != "Gnome":
+        return ""
+    else:
+        sub = roll("1d10")
+        if sub <= 2:
+            return "Deep"
+        elif sub <= 5:
+            return "Forest"
+        return "Rock"
+
+def half_elf_sub(prace):
+    if prace != "Half-Elf":
+        return ""
+    else:
+        sub = roll("1d20")
+        if sub <= 2:
+            return "Half Eladrin"
+        elif sub <= 4:
+            return "Half Drow"
+        elif sub <= 8:
+            return "Half Wood Elf"
+        elif sub <= 13:
+            return "Half High Elf"
+        return "Unkown Elven heritage"
+
+def dragonborn_sub(prace):
+    if prace != "Dragonborn":
+        return ""
+    else:
+        sub = roll("1d10")
+        if sub == 1:
+            return "Black"
+        elif sub == 2:
+            return "Blue"
+        elif sub == 3:
+            return "Brass"
+        elif sub == 4:
+            return "Bronze"
+        elif sub == 5:
+            return "Copper"
+        elif sub == 6:
+            return "Gold"
+        elif sub == 7:
+            return "Green"
+        elif sub == 8:
+            return "Red"
+        elif sub == 9:
+            return "Silver"
+        return "White"
+
+def tiefling_sub(prace):
+    if prace != "Tiefling":
+        return ""
+    else:
+        sub = roll("1d12")
+        if sub == 1:
+            return "Blood of Asmodeus (Standard)"
+        elif sub == 2:
+            return "Blood of Baalzebul"
+        elif sub == 3:
+            return "Blood of Dispater"
+        elif sub == 4:
+            return "Feral"
+        elif sub == 5:
+            return "Feral (Variant)"
+        elif sub == 6:
+            return "Blood of Fierna"
+        elif sub == 7:
+            return "Blood of Glasya"
+        elif sub == 8:
+            return "Blood of Levistus"
+        elif sub == 9:
+            return "Blood of Mammon"
+        elif sub == 10:
+            return "Blood of Mephistopheles"
+        elif sub == 11:
+            return "Unknown (Variant)"
+        return "Blood of Zariel"
 
 primary_race = ""
+sub_race = ""
 
-if primary_roll <= primary_table["HUMAN"]:
-    primary_race = "Human: " + human_sub()
-elif primary_roll <= primary_table["DWARF"]:
-    primary_race = "Dwarf: " + dwarf_sub()
-elif primary_roll <= primary_table["ELF"]:
-    primary_race = "Elf: " + elf_sub()
-elif primary_roll <= primary_table["HALFLING"]:
-    primary_race = "Halfling: " + halfling_sub()
-elif primary_roll <= primary_table["GNOME"]:
-    primary_race = "Gnome: " + gnome_sub() 
-elif primary_roll <= primary_table["HALF-ELF"]:
-    primary_race = "Half-Elf: " + half_elf_sub()
-elif primary_roll <= primary_table["GOLIATH"]:
-    primary_race = "Goliath"
-elif primary_roll <= primary_table["DRAGONBORN"]:
-    primary_race = "Dragonborn: Color -  " + dragonborn_color()
-elif primary_roll <= primary_table["HALF-ORC"]:
-    primary_race = "Half-Orc"
-elif primary_roll <= primary_table["TIEFLING"]:
-    primary_race = "Tiefling"
-elif primary_roll <= primary_table["AASIMAR"]:
-    primary_race = "Aasimar"
-elif primary_roll <= primary_table["AARAKOCRA"]:
-    primary_race = "Aarakocra"
-elif primary_roll <= primary_table["GENASI"]:
-    primary_race = "Genasi"
+primary_race = race(primary_roll, phb_race_table)
+sub_race = subrace(primary_race)
 
-formattedRace = " {} - " + primary_race
+race = primary_race + ": " + sub_race
+
+formattedRace = " {} - " + race
 
 print()
 print(formattedRace.format(primary_roll))
 print()
-
 
